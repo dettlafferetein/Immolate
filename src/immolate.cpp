e34a0fcaffae8b279eefe4f70362f087e3dbace8
Dettlaff <90787612+dettlafferetein@users.noreply.github.com>
2025-04-28 16:56:06 -0400

    .

--- src/immolate.cpp
+++ src/immolate.cpp
@@ -7,7 +7,12 @@
 Item BRAINSTORM_PACK = Item::RETRY;
 Item BRAINSTORM_TAG = Item::Charm_Tag;
 Item BRAINSTORM_VOUCHER = Item::RETRY;
+Item BRAINSTORM_RAREJOKER = Item::RETRY;
+Item BRAINSTORM_UNCOMMONJOKER = Item::RETRY;
 long BRAINSTORM_SOULS = 1;
+bool BRAINSTORM_OBSERVATORY = false;
+bool BRAINSTORM_PERKEO = false;
+bool BRAINSTORM_NEGATIVE5TO8 = false;
 
 long filter(Instance inst) {
     if (BRAINSTORM_PACK != Item::RETRY) {
@@ -28,6 +33,19 @@ long filter(Instance inst) {
             return 0;
         }
     }
+    
+    if (BRAINSTORM_RAREJOKER != Item::RETRY) {
+        if (inst.nextJoker(ItemSource::Rare_Tag, 1, false).joker != BRAINSTORM_RAREJOKER) {
+            return 0;
+        }
+    }
+
+    if (BRAINSTORM_UNCOMMONJOKER != Item::RETRY) {
+        if (inst.nextJoker(ItemSource::Uncommon_Tag, 1, false).joker != BRAINSTORM_UNCOMMONJOKER) {
+            return 0;
+        }
+    }
+
     if (BRAINSTORM_SOULS > 0) {
         for (int i = 1; i <= BRAINSTORM_SOULS; i++) {
             auto tarots = inst.nextArcanaPack(5, 1); //Mega Arcana Pack, assumed from a Charm Tag
@@ -43,25 +61,88 @@ long filter(Instance inst) {
             }
         }
     }
+
+    if (BRAINSTORM_OBSERVATORY) {
+        if (inst.nextVoucher(1) == Item::Telescope) {
+            inst.activateVoucher(Item::Telescope);
+            if (inst.nextVoucher(2) != Item::Observatory) return 0;
+        } else return 0;
+    }
+
+    if (BRAINSTORM_PERKEO) {
+        int antes[5] = {1, 1, 2, 2, 2};
+        bool foundperk = false;
+        for (int i = 0; i < 5; i++) {
+            Pack pack = packInfo(inst.nextPack(antes[i]));
+            std::vector<Item> packContents;
+            if (pack.type == Item::Arcana_Pack) {
+                packContents = inst.nextArcanaPack(pack.size, antes[i]);
+            } else if (pack.type == Item::Spectral_Pack) {
+                packContents = inst.nextSpectralPack(pack.size, antes[i]);
+            } else continue;
+            
+            for (int x = 0; x < pack.size; x++) {
+                if (packContents[x] == Item::The_Soul && inst.nextJoker(ItemSource::Soul, antes[i], true).joker == Item::Perkeo) {
+                    foundperk = true;
+                    break;
+                }
+            }
+
+            if (foundperk) {
+                break;
+            }
+        }
+
+        if (!foundperk) {
+            return 0;
+        }
+    }
+
+    if (BRAINSTORM_NEGATIVE5TO8) {
+        bool foundneg = false;
+        for (int i = 5; i <= 7; i++) {
+            if (inst.nextTag(i) == Item::Negative_Tag) {
+                foundneg = true;
+                break;
+            }
+            if (inst.nextTag(i) == Item::Negative_Tag) {
+                foundneg = true;
+                break;
+            }
+        }
+        if (inst.nextTag(8) == Item::Negative_Tag) {
+            foundneg = true;
+        }
+        if (!foundneg) {
+            return 0;
+        }
+    }
 };
 
-IMMOLATE_API std::string brainstorm_cpp(std::string seed, std::string voucher, std::string pack, std::string tag, double souls) {
+IMMOLATE_API std::string brainstorm_cpp(std::string seed, std::string voucher, std::string pack, std::string tag, std::string rarejoker, std::string uncommonjoker, double souls, bool observatory, bool perkeo, bool negative5to8) {
     BRAINSTORM_PACK = stringToItem(pack);
     BRAINSTORM_TAG = stringToItem(tag);
     BRAINSTORM_VOUCHER = stringToItem(voucher);
+    BRAINSTORM_RAREJOKER = stringToItem(rarejoker);
+    BRAINSTORM_UNCOMMONJOKER = stringToItem(uncommonjoker);
     BRAINSTORM_SOULS = souls;
+    BRAINSTORM_OBSERVATORY = observatory;
+    BRAINSTORM_PERKEO = perkeo;
+    BRAINSTORM_NEGATIVE5TO8 = negative5to8;
     Search search(filter, seed, 1, 100000000);
     search.exitOnFind = true;
     return search.search();
 }
 
 extern "C" {
-    IMMOLATE_API const char* brainstorm(const char* seed, const char* voucher, const char* pack, const char* tag, double souls) {
+    IMMOLATE_API const char* brainstorm(const char* seed, const char* voucher, const char* pack, const char* tag, const char* rarejoker, const char* uncommonjoker, double souls, bool observatory, bool perkeo, bool negative5to8) {
         std::string cpp_seed(seed);
         std::string cpp_pack(pack);
         std::string cpp_voucher(voucher);
         std::string cpp_tag(tag);
-        std::string result = brainstorm_cpp(cpp_seed, cpp_voucher, cpp_pack, cpp_tag, souls);
+        std::string cpp_rarejoker(rarejoker);
+        std::string cpp_uncommonjoker(uncommonjoker);
+        std::string result = brainstorm_cpp(cpp_seed, cpp_voucher, cpp_pack, cpp_tag, cpp_rarejoker, cpp_uncommonjoker, souls, observatory, perkeo, negative5to8);
         
         char* c_result = (char*)malloc(result.length() + 1);
         strcpy(c_result, result.c_str());
--- src/immolate.hpp
+++ src/immolate.hpp
@@ -16,9 +16,9 @@
 #endif
 
 // Declare the functions with IMMOLATE_API
-IMMOLATE_API std::string brainstorm_cpp(std::string seed, std::string voucher, std::string pack, std::string tag, double souls);
+IMMOLATE_API std::string brainstorm_cpp(std::string seed, std::string voucher, std::string pack, std::string tag, std::string rarejoker, std::string uncommonjoker, double souls, bool observatory, bool perkeo, bool negative5to8);
 extern "C" {
-    IMMOLATE_API const char* brainstorm(const char* seed, const char* voucher, const char* pack, const char* tag, double souls);
+    IMMOLATE_API const char* brainstorm(const char* seed, const char* voucher, const char* pack, const char* tag, const char* rarejoker, const char* uncommonjoker, double souls, bool observatory, bool perkeo, bool negative5to8);
     IMMOLATE_API void free_result(const char* result);
 }
 
